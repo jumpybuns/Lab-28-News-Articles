6fdede84726e417355dab06a271c40b1f2c8db56
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-03 13:38:21 -0800

    remove results from getArticle service

--- src/services/getArticle.js
+++ src/services/getArticle.js
@@ -1,7 +1,5 @@
 export const getArticle = (title) => {
   return fetch(
     `https://newsapi.org/v2/everything?${title}&apiKey=${process.env.NEWS_API_KEY}`
-  )
-    .then((res) => res.json())
-    .then((json) => json.results);
+  ).then((res) => res.json());
 };
