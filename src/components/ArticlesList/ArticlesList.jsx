607e7de81f081dabb4c85e6d1569682edf4e6545
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-03 14:05:40 -0800

    add ArticleList props

--- src/components/ArticlesList/ArticlesList.jsx
+++ src/components/ArticlesList/ArticlesList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Article from '../Article/Article';
+import PropTypes from 'prop-types';
 
 const ArticlesList = ({ articles }) => {
   const articleElements = articles.map((article) => (
@@ -9,4 +10,15 @@ const ArticlesList = ({ articles }) => {
   ));
 
   return <ul>{articleElements}</ul>;
+};
+
+ArticlesList.propTypes = {
+  articles: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+    }).isRequired
+  ),
 };
