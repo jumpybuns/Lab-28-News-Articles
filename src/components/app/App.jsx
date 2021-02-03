a6dc26d5b723377c253ce1ff5631e65a8596d035
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-03 13:43:12 -0800

    added route to add.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={(routerProps) => <List {...routerProps} />}
+            />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
